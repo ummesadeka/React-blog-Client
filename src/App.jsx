f2a8b55fc6b75d095f06b980db896e7d5a7ac79a
ummesadeka <sadekaahmed53@gmail.com>
2021-11-14 23:07:14 +0600

    header done

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import Topbar from "./Components/topbar/Topbar";
+import Home from "./pages/home/Home";
 
 function App() {
   return (
     <div className="App">
       <Topbar/>
+      <Home/>
     </div>
   );
 }
